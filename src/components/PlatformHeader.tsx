9b9e1f2cb6517c4ba1e33066c6972536ea0cbea7
Ahmetsilkin <ahmetsilkin01@gmail.com>
2026-08-08 13:27:58 +0300

    Delete src directory

--- src/components/PlatformHeader.tsx
+++
@@ -1,2 +0,0 @@
-import Link from'next/link';
-export default function PlatformHeader(){return <header className="platformHeader"><Link className="platformLogo" href="/"><span>Y</span><b>YONTUM</b></Link><nav><Link href="/giris">Giriş Yap</Link><Link className="navCta" href="/kayit">Ücretsiz Başla →</Link></nav></header>}
